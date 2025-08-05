1ed009e6937022ffd23aa26257a4d8e41da3e281
lingting <sunlisten.gzm@foxmail.com>
2025-08-05 17:01:39 +0800

    :art: 重写为基于文件的单进程锁

--- README.md
+++ README.md
@@ -6,8 +6,31 @@
 
 ```rust
 
+fn create_single(path: PathBuf, info: &str) -> AnyResult<Option<Single>> {
+    let single = Single::create(path, info)?;
+    if !single.is_single {
+        log::error!("存在已启动进程: {}", single.pid.unwrap_or(0));
+        log::error!("已启动进程info: {}", single.info);
+        Err(Box::new(BizError::SingleRunning))
+    } else {
+        Ok(Some(single))
+    }
+}
+
+fn main() ->AnyResult<()> {
+    let lock_path = "/tmp/single.lock";
+    let mut o_single = create_single(lock_path, "single info")?;
+    
+    // on close
+    let o = o_single.take();
+    if let Some(single) = o {
+        // 如果要同时兼容 root和非root权限启动, 需要在释放后删除文件. 避免由于权限导致异常
+        let path = single.path.clone();
+        let path_info = single.path_info.clone();
+        drop(single);
+        let _ = file::delete(&path);
+        let _ = file::delete(&path_info);
+    }
+}
+
 ```
-
-### 致谢
-
-- 感谢 (https://github.com/WLBF/single-instance)[single-instance] 仓库开发人员
--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,124 @@
-#[cfg(target_os = "macos")]
-mod mac;
-#[cfg(any(target_os = "linux", target_os = "android"))]
-mod unix;
-#[cfg(target_os = "windows")]
-mod windows;
-
+use fs2::FileExt;
 use std::error::Error;
-
-type AnyResult<T> = Result<T, Box<dyn Error>>;
+use std::fs::{create_dir_all, File, OpenOptions};
+use std::io::{Read, Write};
+use std::path::{Path, PathBuf};
+use std::process;
 
 #[derive(thiserror::Error, Debug)]
 pub enum SingleError {
-    #[error("invalid check single!")]
-    InvalidCheck,
+    #[error("pid and info write err!")]
+    SingleWrite,
 }
 
-trait SingleHandler
-where
-    Self: Drop,
-{}
+type AnyResult<T> = Result<T, Box<dyn Error>>;
 
 pub struct Single {
-    pub key: String,
     pub is_single: bool,
-    _handler: Option<Box<dyn SingleHandler>>,
+    pub pid: Option<u32>,
+    pub info: String,
+    pub path: String,
+    pub path_info: String,
+    file: Option<File>,
+}
+
+fn try_unique<P: AsRef<Path>>(p: P) -> AnyResult<File> {
+    let mut options = OpenOptions::new();
+    options.write(true).create(true);
+
+    #[cfg(target_os = "windows")]
+    {
+        use std::os::windows::fs::OpenOptionsExt;
+        options
+            .share_mode(0x0)
+            .attributes(0)
+            .security_qos_flags(0x0)
+            .custom_flags(0x0)
+            .access_mode(0xC0000000);
+    }
+    #[cfg(not(target_os = "windows"))]
+    {
+        use std::os::unix::fs::OpenOptionsExt;
+        options.mode(0o444);
+    }
+
+    let file = options.open(p)?;
+    FileExt::lock_exclusive(&file)?;
+    Ok(file)
+}
+
+fn write(path: &str, pid: u32, info: &str) -> AnyResult<()> {
+    let mut file = File::create(path)?;
+    let content = format!("{}\n{}", pid, info);
+
+    // 写入PID和信息
+    file.set_len(0)?; // 截断文件
+    file.write_all(content.as_bytes())?;
+    file.flush()?;
+    file.sync_all()?;
+    Ok(())
 }
 
 impl Single {
-    /// 根据key新建实例.
-    pub fn new(key: &str) -> AnyResult<Self> {
-        #[cfg(target_os = "windows")]
-        let option: Option<Box<dyn SingleHandler>> = windows::new(key)?;
-        #[cfg(target_os = "macos")]
-        let option = mac::new(key)?;
-        #[cfg(any(target_os = "linux", target_os = "android"))]
-        let option = unix::new(key)?;
+    pub fn create<P: AsRef<Path>>(p: P, info: &str) -> AnyResult<Single> {
+        let path_lock = p.as_ref();
+        if let Some(parent) = path_lock.parent() {
+            create_dir_all(parent)?;
+        }
+        let p_str = path_lock.to_str().expect("get path err").to_string();
+        let path_info = format!("{}.info", p_str);
 
+        match try_unique(path_lock) {
+            Ok(lock) => {
+                let pid = process::id();
+
+                match write(&path_info, pid, info) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        log::error!("write pid and info to {} err! {}", path_info, e);
+                        return Err(Box::new(SingleError::SingleWrite));
+                    }
+                }
+
+                return Ok(Single {
+                    is_single: true,
+                    pid: Some(pid),
+                    info: info.to_string(),
+                    path: p_str,
+                    path_info,
+                    file: Some(lock),
+                });
+            }
+            Err(e) => {
+                log::error!("获取独占锁异常! {}", e)
+            }
+        }
+
+        let mut options = OpenOptions::new();
+        options.read(true);
+
+        let mut file = options.open(&path_info)?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+        let (pid_str, info) = content.split_once('\n').unwrap_or(("0", &content));
+        let pid = pid_str.parse::<u32>().ok();
         Ok(Single {
-            key: key.to_string(),
-            is_single: option.is_some(),
-            _handler: option,
+            is_single: false,
+            pid,
+            info: info.to_string(),
+            path: p_str,
+            path_info,
+            file: None,
         })
+    }
+}
+
+impl Drop for Single {
+    fn drop(&mut self) {
+        let option = self.file.take();
+        if let Some(file) = option {
+            let _ = FileExt::unlock(&file);
+            drop(file);
+        }
     }
 }
